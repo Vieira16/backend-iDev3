249a94bacb4a137876b1338758a8137a5603504d
Vieira16 <rafaelcevieira8@gmail.com>
2025-02-18 09:23:42 -0300

    atwarwq

---
+++ aula 3/index.js
@@ -0,0 +1,32 @@
+const express = require("./user");
+const userService = require('./userService');
+
+const app = express(); // nome qualquer para express
+app.use(express.json); // vou habilitar json no express
+
+app.post("/users", (req, res) =>{
+    const {nome, email} = req.body;
+    if(!nome || !email){
+        return res.status(400).json({error: "Nome e Email são Obrigatórios"})
+    }
+
+    const user = userService.addUser(nome, email);
+    res.status(200).json({user});
+
+    }   
+)
+
+// rota para listar todos os usuarios
+
+app.get("/users", (req, res) =>{
+    res.json(userService.getUsers());
+    }
+)
+
+const port = 3000;
+app.listen(port,() =>{
+    console.log("Servidor rodando na porta: ", port);
+
+    }
+)
+
--- aula 3/user.js
+++ aula 3/user.js
@@ -1,11 +1,11 @@
 class User{
     constructor(id, nome, email){
-        this.id = id;
-        this.nome = nome;
-        this.email = email;
+        this.id = id; // id do usuario
+        this.nome = nome; // nome do usuario
+        this.email = email; // email do usuario
 
     }
 
 }
 
-modulo.exports = User;
+modulo.exports = User; // exportar o modulo
